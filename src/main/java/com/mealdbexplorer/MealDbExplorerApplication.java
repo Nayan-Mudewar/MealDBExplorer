fb31b55558b508570e2be23d83ce5427e16611ac
Nayan-Mudewar <nayanmudewar2002@gmail.com>
2025-12-23 23:36:29 +0530

    Initial Spring Boot setup with health endpoint

---
+++ src/main/java/com/mealdbexplorer/MealDbExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.mealdbexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealDbExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealDbExplorerApplication.class, args);
+	}
+
+}
